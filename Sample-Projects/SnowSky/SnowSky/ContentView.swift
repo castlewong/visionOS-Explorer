2c8c59ba44e3509cc3fca8af938aa7e6085fe220
Wilbur Wong <swy0114@qq.com>
2023-07-30 11:24:22 +0800

    Let it snow.

--- Sample-Projects/SnowSky/SnowSky/ContentView.swift
+++ Sample-Projects/SnowSky/SnowSky/ContentView.swift
@@ -12,7 +12,7 @@ import RealityKitContent
 struct ContentView: View {
 
     @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
+//    @State private var immersiveSpaceIsShown = false
 
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
@@ -21,20 +21,31 @@ struct ContentView: View {
         
         NavigationSplitView {
             List{
-                Text("Tem")
+                Text("item")
             }
             .navigationTitle("Sidebar")
         } detail: {
             VStack{
                 Model3D(named: "Scene", bundle: realityKitContentBundle)
                     .padding(.bottom, 50)
-                Text("Hello")
+                
+                Text("Let Sky Snow")
+                
                 Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
                     .toggleStyle(.button)
                     .padding(.top, 50)
             }
             .navigationTitle("Content")
             .padding()
+        }
+        .onChange(of: showImmersiveSpace) { _, newValue in
+            Task {
+                if newValue {
+                    await openImmersiveSpace(id: "ImmersiveSpace")
+                } else {
+                    
+                }
+            }
         }
     }
 }
